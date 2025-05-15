2f1f3d818d9e94e5f576bffe7f3ce19656be02f0
heimsharon <heimsharon@hotmail.com>
2025-05-15 00:46:48 -0400

    added auth log in with jwt via env

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,10 +1,37 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 
+dotenv.config()
 interface JwtPayload {
-  username: string;
+    username: string;
 }
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  // TODO: verify the token exists and add the user data to the request object
+export const authenticateToken = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+        const secretKey = process.env.JWT_SECRET_KEY || '';
+
+        try {
+            jwt.verify(token, secretKey, (err, user) => {
+                if (err) {
+                    return res.sendStatus(403); // Forbidden
+                }
+
+                req.user = user as JwtPayload;
+                return next();
+            });
+        } catch (error) {
+            console.error('Error verifying token:', error);
+            return res.sendStatus(500); // Internal Server Error
+        }
+    } else {
+        return res.sendStatus(401); // Unauthorized
+    }
 };
