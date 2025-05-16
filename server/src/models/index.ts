9e8854222682f76cfafac49e12f1c6d5ed2037e0
heimsharon <heimsharon@hotmail.com>
2025-05-15 21:08:36 -0400

    updates for deploy

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -7,13 +7,19 @@ import { TicketFactory } from './ticket.js';
 
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
-    host: 'localhost',
-    dialect: 'postgres',
-    dialectOptions: {
-      decimalNumbers: true,
-    },
-  });
+  : new Sequelize(
+      process.env.DB_NAME || '',
+      process.env.DB_USER || '',
+      process.env.DB_PASSWORD || '',
+      {
+        host: process.env.DB_HOST || 'localhost',
+        port: Number(process.env.DB_PORT) || 5432,
+        dialect: 'postgres',
+        dialectOptions: {
+          decimalNumbers: true,
+        },
+      }
+    );
 
 const User = UserFactory(sequelize);
 const Ticket = TicketFactory(sequelize);
