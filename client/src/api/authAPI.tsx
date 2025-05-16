aaf8cabb1275119f60d667bfb28403efb696b547
heimsharon <heimsharon@hotmail.com>
2025-05-16 00:34:19 -0400

    removing dev client api

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,32 +1,27 @@
-import { UserLogin, LoginResponse } from "../interfaces/UserLogin";
+import { UserLogin, LoginResponse } from '../interfaces/UserLogin';
 
-const BASE_URL = process.env.REACT_APP_API_URL ||
-  'http://localhost:3001';
 
-const login = async (userInfo: UserLogin):
-  Promise<LoginResponse> => {
+const login = async (userInfo: UserLogin): Promise<LoginResponse> => {
+    try {
+        const response = await fetch('/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userInfo),
+        });
 
-  try {
-    const response = await fetch(`${BASE_URL}/auth/login`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(userInfo)
-    });
+        if (!response.ok) {
+            const errorData = await response.json();
+            throw new Error(`Error: ${errorData.message}`);
+        }
 
-    if (!response.ok) {
-      const errorData = await response.json();
-      throw new Error(`Error: ${errorData.message}`);
+        const data = await response.json();
+        return data;
+    } catch (err) {
+        console.error('Error from user login:', err);
+        throw new Error('Could not fetch user info');
     }
-
-    const data = await response.json();
-    return data;
-
-  } catch (err) {
-    console.error('Error from user login:', err);
-    throw new Error('Could not fetch user info');
-  }
 };
 
 export { login };
