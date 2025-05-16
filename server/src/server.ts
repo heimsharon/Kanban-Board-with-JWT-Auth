fb0f9b2448ff73e0521155a17d5a68052871faad
heimsharon <heimsharon@hotmail.com>
2025-05-15 23:21:28 -0400

    server update

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,7 @@ const forceDatabaseRefresh = false;
 import dotenv from 'dotenv';
 dotenv.config();
 
+import path from 'path';
 import express from 'express';
 import cors from 'cors';
 import routes from './routes/index.js';
@@ -12,18 +13,24 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Serves static files in the entire client's dist folder
-app.use(express.static('client/dist')); // Updated path
+app.use(express.static(path.join(__dirname, '../../client/dist'))); // Updated path
 
-app.use(cors({
-  origin: process.env.CLIENT_URL, // Allow requests from the frontend URL
-  credentials: true, // Allow cookies and credentials
-}));
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL, // Allow requests from the frontend URL
+        credentials: true, // Allow cookies and credentials
+    })
+);
 
 app.use(express.json());
 app.use(routes);
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+});
+
 sequelize.sync({ force: forceDatabaseRefresh }).then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-  });
+    app.listen(PORT, () => {
+        console.log(`Server is listening on port ${PORT}`);
+    });
 });
