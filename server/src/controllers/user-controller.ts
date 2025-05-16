51af0e30dba11169137fb1440a8ec6148e41cdf6
heimsharon <heimsharon@hotmail.com>
2025-05-16 00:53:49 -0400

    auth route and control updates

--- server/src/controllers/user-controller.ts
+++ server/src/controllers/user-controller.ts
@@ -1,99 +1,86 @@
 import { Request, Response } from 'express';
-import { User } from '../models/user.js';
+import { User } from '../models/index.js';
+
 
 // GET /Users
-export const getAllUsers = async (
-  _req: Request,
-  res: Response
-) => {
+export const getAllUsers = async (_req: Request, res: Response) => {
+    try {
+        const users = await User.findAll({
+            attributes: { exclude: ['password'] },
+        });
 
-  try {
-    const users = await User.findAll({
-      attributes: { exclude: ['password'] }
-    });
-
-    res.json(users);
-  } catch (error: any) {
-    res.status(500).json({ message: error.message });
-  }
+        res.json(users);
+    } catch (error: any) {
+        res.status(500).json({ message: error.message });
+    }
 };
 
 // GET /Users/:id
-export const getUserById = async (
-  req: Request,
-  res: Response
-) => {
-  const { id } = req.params;
+export const getUserById = async (req: Request, res: Response) => {
+    const { id } = req.params;
 
-  try {
-    const user = await User.findByPk(id, {
-      attributes: { exclude: ['password'] }
-    });
+    try {
+        const user = await User.findByPk(id, {
+            attributes: { exclude: ['password'] },
+        });
 
-    if (user) {
-      res.json(user);
-    } else {
-      res.status(404).json({ message: 'User not found' });
+        if (user) {
+            res.json(user);
+        } else {
+            res.status(404).json({ message: 'User not found' });
+        }
+    } catch (error: any) {
+        res.status(500).json({ message: error.message });
     }
-  } catch (error: any) {
-    res.status(500).json({ message: error.message });
-  }
 };
 
 // POST /Users
-export const createUser = async (
-  req: Request,
-  res: Response
-) => {
-  const { username, password } = req.body;
+export const createUser = async (req: Request, res: Response) => {
+    const { username, password } = req.body;
 
-  try {
-    const newUser = await User.create({ username, password });
-    res.status(201).json(newUser);
-  } catch (error: any) {
-    res.status(400).json({ message: error.message });
-  }
+    try {
+        const newUser = await User.create({ username, password });
+        res.status(201).json(newUser);
+    } catch (error: any) {
+        res.status(400).json({ message: error.message });
+    }
 };
 
 // PUT /Users/:id
-export const updateUser = async (
-  req: Request,
-  res: Response
-) => {
-  const { id } = req.params;
-  const { username, password } = req.body;
+export const updateUser = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { username, password } = req.body;
 
-  try {
-    const user = await User.findByPk(id);
-    if (user) {
-      user.username = username;
-      user.password = password;
-      await user.save();
-      res.json(user);
-    } else {
-      res.status(404).json({ message: 'User not found' });
+    try {
+        const user = await User.findByPk(id);
+        if (user) {
+            user.username = username;
+            user.password = password;
+            await user.save();
+            res.json(user);
+        } else {
+            res.status(404).json({ message: 'User not found' });
+        }
+    } catch (error: any) {
+        res.status(400).json({ message: error.message });
     }
-  } catch (error: any) {
-    res.status(400).json({ message: error.message });
-  }
 };
 
 // DELETE /Users/:id
-export const deleteUser = async (
-  req: Request,
-  res: Response
-) => {
-  const { id } = req.params;
+export const deleteUser = async (req: Request, res: Response) => {
+    const { id } = req.params;
 
-  try {
-    const user = await User.findByPk(id);
-    if (user) {
-      await user.destroy();
-      res.json({ message: 'User deleted' });
-    } else {
-      res.status(404).json({ message: 'User not found' });
+    try {
+        const user = await User.findByPk(id);
+        if (user) {
+            await user.destroy();
+            res.json({ message: 'User deleted' });
+        } else {
+            res.status(404).json({ message: 'User not found' });
+        }
+    } catch (error: any) {
+        res.status(500).json({ message: error.message });
     }
-  } catch (error: any) {
-    res.status(500).json({ message: error.message });
-  }
 };
+
+
--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,11 +1,23 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
+import { UserFactory, User } from './user.js';
+import { TicketFactory, Ticket } from './ticket.js';
 
 dotenv.config();
 
 export const sequelize = new Sequelize(process.env.DB_URL || '', {
-  dialect: 'postgres',
-  dialectOptions: {
-    decimalNumbers: true,
-  },
+    dialect: 'postgres',
+    dialectOptions: {
+        decimalNumbers: true,
+    },
 });
+
+// Initialize models
+UserFactory(sequelize);
+TicketFactory(sequelize);
+
+// Define associations ONCE here
+Ticket.belongsTo(User, { as: 'assignedUser', foreignKey: 'assignedUserId' });
+User.hasMany(Ticket, { as: 'assignedTickets', foreignKey: 'assignedUserId' });
+
+export { User, Ticket };
