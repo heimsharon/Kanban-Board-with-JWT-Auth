2236b3a976ac97ccbdf5bf2271f26357a53a2cb6
heimsharon <heimsharon@hotmail.com>
2025-05-15 22:37:15 -0400

    update api url, cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import cors from 'cors';
 import routes from './routes/index.js';
 import { sequelize } from './models/index.js';
 
@@ -12,6 +13,11 @@ const PORT = process.env.PORT || 3001;
 
 // Serves static files in the entire client's dist folder
 app.use(express.static('client/dist')); // Updated path
+
+app.use(cors({
+  origin: process.env.CLIENT_URL, // Allow requests from the frontend URL
+  credentials: true, // Allow cookies and credentials
+}));
 
 app.use(express.json());
 app.use(routes);
